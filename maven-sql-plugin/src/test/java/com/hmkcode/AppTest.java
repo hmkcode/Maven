6fede0527bbcc6734f8cef9b19d3ec95bb1b135d
hmkcode <hmkcode@gmail.com>
2013-05-17 22:43:08 +0300

    sql maven plugin
    sql maven plugin

---
+++ maven-sql-plugin/src/test/java/com/hmkcode/AppTest.java
@@ -0,0 +1,19 @@
+package com.hmkcode;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+
+
+
+/**
+ * Unit test for simple App.
+ */
+@RunWith(JUnit4.class)
+public class AppTest {
+	@Test
+	public void doTest(){
+		
+	}
+}
---
+++ maven-sql-plugin/src/test/sql/test-data.sql
@@ -0,0 +1,6 @@
+INSERT INTO person (name) VALUES ('person-1');
+INSERT INTO person (name) VALUES ('person-2');
+INSERT INTO person (name) VALUES ('person-3');
+INSERT INTO person (name) VALUES ('person-4');
+INSERT INTO person (name) VALUES ('person-5');
+INSERT INTO person (name) VALUES ('person-6');
